e9c445f63ea18e0f217fe7897443fc187c76f6db
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-08-31 10:52:00 -0500

    df

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,12 +4,12 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, comma
   throw false
   }
   let pesan = args.join` `
-  let oi = `*🛍️https://chat.whatsapp.com/HYebmThadjt9FPzLcCGocf ${pesan}`
-  let teks = `*⬇️Venta de 🤖 ,regedits 🖥️📱 ,sensibilidades📱 ,+ 30 metodos ,cuentas ff y mass⬇️*
-   ${oi}\n\n🛍️ *@Ale.izn_20:*\n`
+  let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴: ${pesan}`
+  let teks = `*⺀👻Madrugar se hace más fácil contigo a mi lado⺀*
+   ${oi}\n\n💋 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
   for (let mem of participants) {
   teks += `⭐ @${mem.id.split('@')[0]}\n`}
-  teks += `𝐁𝐎𝐓𝐂𝐈𝐓𝐎 𝐂𝐀𝐆𝐀 𝐇𝐈𝐄𝐋𝐎`
+  teks += `𝐉𝐞𝐨𝐧𝐁𝐨𝐭`
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
   handler.help = ['tagall <mesaje>','invocar <mesaje>']
